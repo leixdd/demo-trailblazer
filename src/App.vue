a8689e60a1067b2f8388f47ac1e2b42790250948
Lei <lecias.ariel@gmail.com>
2023-11-10 09:19:29 +0800

    change title

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
       <h1
         class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
       >
-        Understand User Flow.
+        HELLO FEU?!
 
         <span class="sm:block"> Increase Conversion. </span>
       </h1>
